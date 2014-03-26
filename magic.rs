00fe963c5f27650d757fd7c684c75bd7db9d5bd6
Daniel Micay <danielmicay@gmail.com>
2014-03-25 20:52:19 -0400

    fix compilation for master

--- magic.rs
+++ magic.rs
@@ -1,16 +1,10 @@
-#[link(name = "magic",
-       vers = "0.1.0",
-       uuid = "201abfd3-0d07-41ab-a6c5-9eb94b318383",
-       url = "https://github.com/thestinger/rust-magic")];
-
+#[crate_id = "magic"];
+#[crate_type = "lib"];
 #[comment = "libmagic bindings"];
 #[license = "MIT"];
-#[crate_type = "lib"];
 
 use std::libc::{c_char, c_int, size_t};
-use std::ptr::is_null;
-use std::str::as_c_str;
-use std::{str, vec};
+use std::str;
 
 enum Magic {}
 
@@ -93,7 +87,8 @@ pub static MAGIC_NO_CHECK_TOKENS: MagicFlag = MagicFlag{flag: 0x100000};
 /// Don't check text encodings
 pub static MAGIC_NO_CHECK_ENCODING: MagicFlag = MagicFlag{flag: 0x200000};
 
-#[link_args = "-lmagic"]
+#[allow(dead_code)]
+#[link(name = "magic")]
 extern "C" {
     fn magic_open(flags: c_int) -> *Magic;
     fn magic_close(cookie: *Magic);
@@ -114,31 +109,31 @@ pub struct Cookie {
 }
 
 impl Drop for Cookie {
-    fn finalize(&self) { unsafe { magic_close(self.cookie) } }
+    fn drop(&mut self) { unsafe { magic_close(self.cookie) } }
 }
 
 impl Cookie {
     pub fn file(&self, filename: &str) -> Option<~str> {
         unsafe {
             let cookie = self.cookie;
-            let s = as_c_str(filename, |filename| magic_file(cookie, filename));
-            if is_null(s) { None } else { Some(str::raw::from_c_str(s)) }
+            let s = filename.with_c_str(|filename| magic_file(cookie, filename));
+            if s.is_null() { None } else { Some(str::raw::from_c_str(s)) }
         }
     }
 
     pub fn buffer(&self, buffer: &[u8]) -> Option<~str> {
         unsafe {
             let buffer_len = buffer.len() as size_t;
-            let pbuffer = vec::raw::to_ptr(buffer);
+            let pbuffer = buffer.as_ptr();
             let s = magic_buffer(self.cookie, pbuffer, buffer_len);
-            if is_null(s) { None } else { Some(str::raw::from_c_str(s)) }
+            if s.is_null() { None } else { Some(str::raw::from_c_str(s)) }
         }
     }
 
     pub fn error(&self) -> Option<~str> {
         unsafe {
             let s = magic_error(self.cookie);
-            if is_null(s) { None } else { Some(str::raw::from_c_str(s)) }
+            if s.is_null() { None } else { Some(str::raw::from_c_str(s)) }
         }
     }
 
@@ -151,59 +146,56 @@ impl Cookie {
     pub fn check(&self, filename: &str) -> bool {
         unsafe {
             let cookie = self.cookie;
-            as_c_str(filename, |filename| magic_check(cookie, filename)) == 0
+            filename.with_c_str(|filename| magic_check(cookie, filename)) == 0
         }
     }
 
     pub fn compile(&self, filename: &str) -> bool {
         unsafe {
             let cookie = self.cookie;
-            as_c_str(filename, |filename| magic_compile(cookie, filename)) == 0
+            filename.with_c_str(|filename| magic_compile(cookie, filename)) == 0
         }
     }
 
     pub fn list(&self, filename: &str) -> bool {
         unsafe {
             let cookie = self.cookie;
-            as_c_str(filename, |filename| magic_list(cookie, filename)) == 0
+            filename.with_c_str(|filename| magic_list(cookie, filename)) == 0
         }
     }
 
     pub fn load(&self, filename: &str) -> bool {
         unsafe {
             let cookie = self.cookie;
-            as_c_str(filename, |filename| magic_load(cookie, filename)) == 0
+            filename.with_c_str(|filename| magic_load(cookie, filename)) == 0
         }
     }
 
     pub fn open(flags: MagicFlag) -> Option<Cookie> {
         unsafe {
-            let cookie = magic_open(flags.flag);
-            if is_null(cookie) { None } else { Some(Cookie{cookie: cookie,}) }
+            let cookie = magic_open(flags.flag | MAGIC_ERROR.flag);
+            if cookie.is_null() { None } else { Some(Cookie{cookie: cookie,}) }
         }
     }
 }
 
 #[cfg(test)]
 mod tests {
-    use super::*;
-    use std::str;
+    use super::{Cookie, MAGIC_NONE, MAGIC_MIME_TYPE, MAGIC_MIME_ENCODING, MAGIC_ERROR};
 
     #[test]
     fn file() {
         let cookie = Cookie::open(MAGIC_NONE).unwrap();
         assert!(cookie.load("/usr/share/file/misc/magic.mgc"));
 
-        assert!(cookie.file("rust-logo-128x128-blk.png").unwrap() ==
-                ~"PNG image data, 128 x 128, 8-bit/color RGBA, non-interlaced");
+        assert_eq!(cookie.file("rust-logo-128x128-blk.png").unwrap(),
+                   ~"PNG image data, 128 x 128, 8-bit/color RGBA, non-interlaced");
 
         cookie.setflags(MAGIC_MIME_TYPE);
-        assert!(cookie.file("rust-logo-128x128-blk.png").unwrap() ==
-                ~"image/png");
+        assert_eq!(cookie.file("rust-logo-128x128-blk.png").unwrap(), ~"image/png");
 
         cookie.setflags(MAGIC_MIME_TYPE | MAGIC_MIME_ENCODING);
-        assert!(cookie.file("rust-logo-128x128-blk.png").unwrap() ==
-                ~"image/png; charset=binary");
+        assert_eq!(cookie.file("rust-logo-128x128-blk.png").unwrap(), ~"image/png; charset=binary");
     }
 
     #[test]
@@ -211,25 +203,20 @@ mod tests {
         let cookie = Cookie::open(MAGIC_NONE).unwrap();
         assert!(cookie.load("/usr/share/file/misc/magic.mgc"));
 
-        let s = ~"#!/usr/bin/env python3\nprint('Hello, world!')";
-        assert!(str::as_bytes(&s, |bytes| {
-          cookie.buffer(*bytes)
-        }).unwrap() == ~"Python script, ASCII text executable");
+        let s = bytes!("#!/usr/bin/env python3\nprint('Hello, world!')");
+        assert_eq!(cookie.buffer(s).unwrap(), ~"Python script, ASCII text executable");
 
         cookie.setflags(MAGIC_MIME_TYPE);
-        assert!(str::as_bytes(&s, |bytes| {
-          cookie.buffer(*bytes)
-        }).unwrap() == ~"text/x-python");
+        assert_eq!(cookie.buffer(s).unwrap(), ~"text/x-python");
     }
 
     #[test]
     fn file_error() {
-        let cookie = Cookie::open(MAGIC_NONE).unwrap();
+        let cookie = Cookie::open(MAGIC_NONE | MAGIC_ERROR).unwrap();
         assert!(cookie.load("/usr/share/file/misc/magic.mgc"));
 
         let ret = cookie.file("non-existent_file.txt");
-        assert!(ret.is_none());
-        assert!(cookie.error().unwrap() ==
-                ~"cannot open `non-existent_file.txt' (No such file or directory)");
+        assert_eq!(ret, None);
+        assert_eq!(cookie.error().unwrap(), ~"cannot stat `non-existent_file.txt' (No such file or directory)");
     }
 }
