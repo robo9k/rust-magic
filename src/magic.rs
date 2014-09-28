719e3dfd33534344757bb39e34c9bc6f2533f0b6
robo9k <robo@9k.lv>
2014-09-29 01:10:05 +0200

    Copy doc from magic.h verbatim for MAGIC_* flags

--- src/magic.rs
+++ src/magic.rs
@@ -8,30 +8,77 @@ enum Magic {}
 
 bitflags! {
     flags Flags: c_int {
+        #[doc = "No flags"]
         static MAGIC_NONE              = 0x000000,
+
+        #[doc = "Turn on debugging"]
         static MAGIC_DEBUG             = 0x000001,
+
+        #[doc = "Follow symlinks"]
         static MAGIC_SYMLINK           = 0x000002,
+
+        #[doc = "Check inside compressed files"]
         static MAGIC_COMPRESS          = 0x000004,
+
+        #[doc = "Look at the contents of devices"]
         static MAGIC_DEVICES           = 0x000008,
+
+        #[doc = "Return the MIME type"]
         static MAGIC_MIME_TYPE         = 0x000010,
+
+        #[doc = "Return all matches"]
         static MAGIC_CONTINUE          = 0x000020,
+
+        #[doc = "Print warnings to stderr"]
         static MAGIC_CHECK             = 0x000040,
+
+        #[doc = "Restore access time on exit"]
         static MAGIC_PRESERVE_ATIME    = 0x000080,
+
+        #[doc = "Don't translate unprintable chars"]
         static MAGIC_RAW               = 0x000100,
+
+        #[doc = "Handle ENOENT etc as real errors"]
         static MAGIC_ERROR             = 0x000200,
+
+        #[doc = "Return the MIME encoding"]
         static MAGIC_MIME_ENCODING     = 0x000400,
+
+        #[doc = "Return the MIME type and encoding"]
         static MAGIC_MIME              = MAGIC_MIME_TYPE.bits
                                        | MAGIC_MIME_ENCODING.bits,
+
+        #[doc = "Return the Apple creator and type"]
         static MAGIC_APPLE             = 0x000800,
+
+        #[doc = "Don't check for compressed files"]
         static MAGIC_NO_CHECK_COMPRESS = 0x001000,
+
+        #[doc = "Don't check for tar files"]
         static MAGIC_NO_CHECK_TAR      = 0x002000,
+
+        #[doc = "Don't check magic entries"]
         static MAGIC_NO_CHECK_SOFT     = 0x004000,
+
+        #[doc = "Don't check application type"]
         static MAGIC_NO_CHECK_APPTYPE  = 0x008000,
+
+        #[doc = "Don't check for elf details"]
         static MAGIC_NO_CHECK_ELF      = 0x010000,
+
+        #[doc = "Don't check for text files"]
         static MAGIC_NO_CHECK_TEXT     = 0x020000,
+
+        #[doc = "Don't check for cdf files"]
         static MAGIC_NO_CHECK_CDF      = 0x040000,
+
+        #[doc = "Don't check tokens"]
         static MAGIC_NO_CHECK_TOKENS   = 0x100000,
+
+        #[doc = "Don't check text encodings"]
         static MAGIC_NO_CHECK_ENCODING = 0x200000,
+
+        #[doc = "No built-in tests; only consult the magic file"]
         static MAGIC_NO_CHECK_BUILTIN  = MAGIC_NO_CHECK_COMPRESS.bits
                                        | MAGIC_NO_CHECK_TAR.bits
                                        | MAGIC_NO_CHECK_APPTYPE.bits
