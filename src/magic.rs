df01825b129954aad9b8c49f43a3c75bb415839f
robo9k <robo@9k.lv>
2014-09-28 16:15:26 +0200

    Fix using tabs instead of spaces for Flags

--- src/magic.rs
+++ src/magic.rs
@@ -8,31 +8,31 @@ enum Magic {}
 
 bitflags! {
     flags Flags: c_int {
-	    static MAGIC_NONE              = 0x000000,
-		static MAGIC_DEBUG             = 0x000001,
-		static MAGIC_SYMLINK           = 0x000002,
-		static MAGIC_COMPRESS          = 0x000004,
-		static MAGIC_DEVICES           = 0x000008,
-		static MAGIC_MIME_TYPE         = 0x000010,
-		static MAGIC_CONTINUE          = 0x000020,
-		static MAGIC_CHECK             = 0x000040,
-		static MAGIC_PRESERVE_ATIME    = 0x000080,
-		static MAGIC_RAW               = 0x000100,
-		static MAGIC_ERROR             = 0x000200,
-		static MAGIC_MIME_ENCODING     = 0x000400,
-		static MAGIC_MIME              = MAGIC_MIME_TYPE.bits
+        static MAGIC_NONE              = 0x000000,
+        static MAGIC_DEBUG             = 0x000001,
+        static MAGIC_SYMLINK           = 0x000002,
+        static MAGIC_COMPRESS          = 0x000004,
+        static MAGIC_DEVICES           = 0x000008,
+        static MAGIC_MIME_TYPE         = 0x000010,
+        static MAGIC_CONTINUE          = 0x000020,
+        static MAGIC_CHECK             = 0x000040,
+        static MAGIC_PRESERVE_ATIME    = 0x000080,
+        static MAGIC_RAW               = 0x000100,
+        static MAGIC_ERROR             = 0x000200,
+        static MAGIC_MIME_ENCODING     = 0x000400,
+        static MAGIC_MIME              = MAGIC_MIME_TYPE.bits
                                        | MAGIC_MIME_ENCODING.bits,
-		static MAGIC_APPLE             = 0x000800,
-		static MAGIC_NO_CHECK_COMPRESS = 0x001000,
-		static MAGIC_NO_CHECK_TAR      = 0x002000,
-		static MAGIC_NO_CHECK_SOFT     = 0x004000,
-		static MAGIC_NO_CHECK_APPTYPE  = 0x008000,
-		static MAGIC_NO_CHECK_ELF      = 0x010000,
-		static MAGIC_NO_CHECK_TEXT     = 0x020000,
-		static MAGIC_NO_CHECK_CDF      = 0x040000,
-		static MAGIC_NO_CHECK_TOKENS   = 0x100000,
-		static MAGIC_NO_CHECK_ENCODING = 0x200000,
-		static MAGIC_NO_CHECK_BUILTIN  = MAGIC_NO_CHECK_COMPRESS.bits
+        static MAGIC_APPLE             = 0x000800,
+        static MAGIC_NO_CHECK_COMPRESS = 0x001000,
+        static MAGIC_NO_CHECK_TAR      = 0x002000,
+        static MAGIC_NO_CHECK_SOFT     = 0x004000,
+        static MAGIC_NO_CHECK_APPTYPE  = 0x008000,
+        static MAGIC_NO_CHECK_ELF      = 0x010000,
+        static MAGIC_NO_CHECK_TEXT     = 0x020000,
+        static MAGIC_NO_CHECK_CDF      = 0x040000,
+        static MAGIC_NO_CHECK_TOKENS   = 0x100000,
+        static MAGIC_NO_CHECK_ENCODING = 0x200000,
+        static MAGIC_NO_CHECK_BUILTIN  = MAGIC_NO_CHECK_COMPRESS.bits
                                        | MAGIC_NO_CHECK_TAR.bits
                                        | MAGIC_NO_CHECK_APPTYPE.bits
                                        | MAGIC_NO_CHECK_ELF.bits
